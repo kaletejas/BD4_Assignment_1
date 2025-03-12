be78a4f4ef55a96c2bcc8a0c0938f56b9c75c137
kaletejas <tejasvijaykale@gmail.com>
2025-03-12 17:49:12 -0400

    added endpoint 1 : to get all restaurants

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { resolve } = require('path');
 const cors = require('cors');
 const { open } = require('sqlite');
+const { resolveSoa } = require('dns');
 const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
@@ -17,6 +18,24 @@ let db;
   });
 })();
 
+//fn 1
+async function fetchAllRestaurants(){
+  let query = 'SELECT * FROM restaurants'
+  let response = await db.all(query, [])
+  return { restaurants : response}
+}
+//Exercise 1: Get All Restaurants
+app.get('/restaurants', async (req,res)=>{
+  try{
+    let results = await fetchAllRestaurants();
+    if(results.restaurants.length === 0){
+      return res.status(404).json({message : 'Restuarants not found'})
+    }
+    res.status(200).json(results)
+  }catch(error){
+    return res.status(500).json({error : error.message})
+  }
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
